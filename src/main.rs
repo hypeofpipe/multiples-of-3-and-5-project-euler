096b00d3ed735fd214519a60a4191e93db0ae6e8
hypeofpipe <hypeofpipe@gmail.com>
2019-02-28 21:54:06 +0100

    Linter fixes

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 below 1000.*/
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Example: divisors [3, 5] maximum nuber below 10 (9)
+
+1. Divide max number by divisors => 9 / 3 = 3; 9 / 5 = 1.8 (omit the rest, left the whole number) = 1
+2. For each that sum, create an array from 1 .. maxnum/divisor [[1, 2, 3], [1]] and for each number in that array
+ multiply it by the divisor [[1 * 3, 2 * 3, 3 * 3], [1 * 5]]
+3. Add the result [[3 + 6 + 9] + [5]] => [18 + 5] => 23
+*/
 
 fn calculate_the_sum(maximum_number: i32) -> i32 {
     let divisors = [3, 5];
 
-    let sum = divisors
-        .iter()
-        .map(|x| [maximum_number / x, x])
-        .map(|x| (1 .. x[0]).map(|y| y * x[1]).fold(0, |acc, y| acc + y))
-        .fold(0, |acc, x| acc + x);
-    println!("{}", sum);
+    let arrays: [i32; 2] = divisors.iter().map(|x| x + 2);
+
+    println!("{:?}", arrays);
 
     4
 }
